98f209f50cb3eecc2e24bd9262dfe86fc4d34a58
dbSoft-SoftwareSolutions <61802513+dbSoft-SoftwareSolutions@users.noreply.github.com>
2020-03-04 22:43:54 +0200

    Add files via upload

---
+++ myData_RequestInvoices/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            var request = new RequestInvoices();
+            var result = await request.MakeRequestAsync("xxxx", "xxxx", "{0}");
+            //Στο Πρώτο πεδίο θέλει το UserID, στο δεύτερο το Subscription
+            // και στο τρίτο το Mark(από ότι καταλαβαίνω το Mark πρέπει να έχει να περιλαμβάνει { } )
+
+            Cursor.Current = Cursors.Default;
+            MessageBox.Show(result);
+        }
+        
+    }
+}
---
+++ myData_RequestInvoices/WindowsFormsApp1/RequestInvoices.cs
@@ -0,0 +1,40 @@
+﻿using Mono.Web;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class RequestInvoices
+    {
+        public  async Task<string> MakeRequestAsync(string UserID, string Subscription, string Mark)
+        {
+            HttpResponseMessage response ;
+         
+            using (var client = new HttpClient())
+            {
+                client.Timeout = TimeSpan.FromSeconds(20);
+                var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+
+                // Request headers
+                client.DefaultRequestHeaders.Add("aade-user-id", UserID);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Subscription);
+
+                // Request parameters
+                queryString["mark"] = Mark;
+
+                var uri = "https://mydata-dev.azure-api.net/RequestInvoices?" + queryString;
+
+                response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    //Read respone
+                }
+            }
+           
+            return response.ToString();
+        }
+    }
+}
